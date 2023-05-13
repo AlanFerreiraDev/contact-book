8e07166a8f17dadd384518b6486fb6e7cee9878e
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-05-13 00:20:47 -0300

    feat: criar hook para verificar tamanho do window global

---
+++ src/hooks/isMobile.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+
+export const useMobileButton = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 512)
+    }
+
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return { isMobile }
+}
